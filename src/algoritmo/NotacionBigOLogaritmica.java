99163151c5c661930c93c772c6ea71bd673e5280
Dav082004 <david.contreras.palacios@gmail.com>
2024-09-08 20:53:13 -0500

    Incluyendo notacion big O logaritmica

---
+++ src/algoritmo/NotacionBigOLogaritmica.java
@@ -0,0 +1,15 @@
+
+package algoritmo;
+
+public class NotacionBigOLogaritmica {
+
+    public static void main(String[] args) {
+        
+        int num = 512;
+        
+        while(num>=1){
+            System.out.println(""+num);
+            num=num/2;
+        }
+    }
+}
